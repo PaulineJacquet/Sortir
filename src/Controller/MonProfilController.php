dce6e454a288f2e24c08e5f3b29a7ab85f94688c
pjacquet2022 <pauline.jacquet2022@campus-eni.fr>
2023-05-09 14:59:35 +0200

    Pauline - Password

--- src/Controller/MonProfilController.php
+++ src/Controller/MonProfilController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Participants;
+use App\Form\MdpType;
 use App\Form\MonProfilType;
+use App\Form\PasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[IsGranted('ROLE_USER')]
 class MonProfilController extends AbstractController
 {
@@ -33,4 +37,28 @@ class MonProfilController extends AbstractController
             'formProfil' => $form->createView(),
         ]);
     }
+    #[Route('/password', name: 'app_password', methods: ['GET', 'POST'])]
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $participant = $this->getUser();
+
+        $form = $this->createForm(MdpType::class,$participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($passwordHasher->isPasswordValid($participant, $form->get('currentPassword')->getData())) {
+                $plainPassword = $form->get('plainPassword')['first']->getData();
+                $newhashedPassword = $passwordHasher->hashPassword($participant, $plainPassword);
+                $participant->setPassword($newhashedPassword);
+                $entityManager->flush();
+                $this->addFlash('success', 'Le mot de passe a été modifié avec succés !');
+                return $this->redirectToRoute('app_mon_profil');
+            }
+        }
+
+        return $this->render('mon_profil/Password.html.twig', [
+           'formMdp' => $form->createView(),
+        ]);
+    }
+
 }
